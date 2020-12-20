161dd4e41b0b7402163206eaf30d44f543cc059f
Nathon Reed - nrwebdev00 <nrwebdev00@gmail.com>
2020-12-20 04:33:53 -0800

    setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import './styles/index.css';
+
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import { Provider } from 'react-redux';
+import store from './store';
+import './bootstrap.min.css';
+import './styles/index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
